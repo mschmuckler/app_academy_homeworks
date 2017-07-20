ce5f0e3330f1f6b4d17070f3e4f26b014678958e
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-07-19 22:33:51 -0400

    Add w3d4 homework

---
+++ w3d4/n_1_buster/app/models/route.rb
@@ -0,0 +1,43 @@
+class Route < ActiveRecord::Base
+  has_many(
+    :buses,
+    class_name: "Bus",
+    foreign_key: :route_id,
+    primary_key: :id
+  )
+  # Create a hash with bus id's as keys and an
+  # array of bus drivers as the corresponding value.
+  #
+  # (e.g., {'1' => ['Joan Lee', 'Charlie McDonald', 'Kevin Quashie'],
+  # '2' => ['Ed Michaels', 'Lisa Frank', 'Sharla Alegria']})
+
+  def n_plus_one_drivers
+    buses = self.buses
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+
+  def better_drivers_query
+    buses = self.buses.includes(:drivers)
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+end
